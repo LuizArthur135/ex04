a4a71ccf7ee1b3b646a21ec4f95fea37916f493e
Luiz Arthur <96485635+LuizArthur135@users.noreply.github.com>
2022-01-01 13:20:19 -0300

    ex04

---
+++ ex04.py
@@ -0,0 +1,10 @@
+list1 = ['Olá', 'levar']
+list2 = ['caro', 'senhor']
+list3 = []
+for cont in range(0, len(list1)):
+    print(list1[cont])
+for nume in range(0, len(list2)):
+    print(list2[cont])
+
+
+#     NÃO CONSEGUI
